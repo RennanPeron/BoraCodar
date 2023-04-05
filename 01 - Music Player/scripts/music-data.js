8730c18e067719844ca75c73a344b07e2a82124a
RennanPeron <rennanperon01@gmail.com>
2023-04-05 19:56:08 -0300

    add scripts

---
+++ 01 - Music Player/scripts/music-data.js
@@ -0,0 +1,11 @@
+let music = {
+    name: 'Music 1',
+    artist: 'Artist 1',
+    musicLength: '3:20',
+}
+
+const playButton = document.querySelector('#play');
+playButton.addEventListener('click', function() {
+    
+})
+
