f2c5e9e8dbca974afeb1c678b30b543c4e2148b8
RennanPeron <rennanperon01@gmail.com>
2023-04-10 20:20:46 -0300

    progress bar script and style

--- 01 - Music Player/scripts/music-data.js
+++ 01 - Music Player/scripts/music-data.js
@@ -1,7 +1,7 @@
 let music = {
     name: 'Music 1',
     artist: 'Artist 1',
-    musicLength: 5,  // 3:20 = 200
+    musicLength: 200,  // 3:20 = 200
 }
 
 let isTimerOn = false
@@ -9,34 +9,39 @@ let currentTime = 0
 
 const playButton = document.querySelector('#play');
 playButton.addEventListener('click', function() {
-    TimerOn(music.musicLength)
+    let timer = music.musicLength
+    TimerOn(timer)
 })
 
 
-function TimerOn(duration) {
-    let timer = duration
+function TimerOn(timer) {
     isTimerOn = !isTimerOn;
+    let remainingTimeElement = document.querySelector(".remaining-time")
+    let progressFillElement = document.querySelector(".progress-bar-fill")
+
+    if(currentTime != timer && currentTime != 0) {
+        timer = currentTime
+    }
     
     let Timer = setInterval(function() {
-        if(!isTimerOn || timer <= 0){
+        if(!isTimerOn || timer < 0){
             isTimerOn = false
             clearInterval(Timer)
         } else {
             // Se for undefined o timer acabou de ativar, se não ele já rolou antes;
-            if(currentTime != timer && currentTime != 0) {
-                timer = currentTime
-            }
             let minutes = parseInt(timer / 60, 10)
             let seconds = parseInt(timer % 60, 10)
             
-            minutes = minutes < 10 ? "0" + minutes : minutes
+            // minutes = minutes < 10 ? "0" + minutes : minutes
             seconds = seconds < 10 ? "0" + seconds : seconds
             
-            console.log(minutes + " : " + seconds)
+            let percentage = parseFloat(100-(100*(timer/music.musicLength))).toFixed(2)
+            
+            progressFillElement.style.width = percentage + "%";
+            remainingTimeElement.innerText = minutes + ":" + seconds;
             
             timer--
             currentTime = timer
         }
-    
     }, 1000)
 }
