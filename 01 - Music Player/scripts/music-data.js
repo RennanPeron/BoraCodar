87d4a02fb1b59c8f7f9aaad0b863036fc790565f
RennanPeron <rennanperon01@gmail.com>
2023-04-13 20:24:25 -0300

    rewind and forward functions added

--- 01 - Music Player/scripts/music-data.js
+++ 01 - Music Player/scripts/music-data.js
@@ -1,14 +1,28 @@
 let vm = Vue.createApp({
     data() {
         return {
-            music: {
+            playlist: [
+                {
                     name: 'Acorda Devinho',
                     artist: 'Banda Rocketseat',
                     lenght: 200,  // 3:20 = 200
                 },
+                {
+                    name: 'Bora Codar Garota',
+                    artist: 'Banda Rocketseat',
+                    lenght: 262,  // 4:37 = 262
+                },
+
+            ],
+            music: {
+                name: 'Acorda Devinho',
+                artist: 'Banda Rocketseat',
+                lenght: 200,
+            },
             isTimerOn: false,
             Timer: '',
             currentTime: 0,
+            index: 0,
         }
     }, 
     methods: {
@@ -19,7 +33,7 @@ let vm = Vue.createApp({
                 this.currentTime = this.music.lenght
             }
 
-            this.Timer = setInterval(this.StartTimer, 500)
+            this.Timer = setInterval(this.StartTimer, 1000)
         },
         TimerOff: function() {
             this.isTimerOn = false
@@ -27,17 +41,46 @@ let vm = Vue.createApp({
         },
         StartTimer: function() {
             this.currentTime--
-        }
+            if(this.currentTime < 0) {
+                this.Forward()
+            }
+        },
+        StopTimer: function() {
+            this.TimerOff()
+            this.currentTime = 0
+        },
+        Forward: function() {
+            this.index++
+            if(this.index == this.playlist.length) {
+                this.index = 0
+            }
+            clearInterval(this.Timer)
+            this.currentTime = 0
+            this.music = this.playlist[this.index]
+            this.TimerOn()
+        },
+        Rewind: function () {
+            this.index--
+            if(this.index == this.playlist.length || this.index < 0) {
+                this.index = 0
+            }
+            this.music = this.playlist[this.index]
+        },
+        TimeConverter: function(time) {
+            let minutes = parseInt(time / 60, 10)
+            let seconds = parseInt(time % 60, 10)
+            
+            seconds = seconds < 10 ? "0" + seconds : seconds
+            
+            return `${minutes}:${seconds}`
+        },
     },
     computed: {
         TimeLeft: function() {
-            let minutes = parseInt(this.currentTime / 60, 10)
-            let seconds = parseInt(this.currentTime % 60, 10)
-            
-            // minutes = minutes < 10 ? "0" + minutes : minutes
-            seconds = seconds < 10 ? "0" + seconds : seconds
-
-            return `${minutes}:${seconds}`
+            return this.TimeConverter(this.currentTime)
+        },
+        MusicLenght: function() {
+            return this.TimeConverter(this.music.lenght)
         },
         BarWidth: function() {
             let width = this.currentTime == 0 ? 0 : parseFloat(100-(100*(this.currentTime/this.music.lenght))).toFixed(2)
