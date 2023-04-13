4b270fd3aa8b78eddd13941a9f68c5cc35812c8d
RennanPeron <rennanperon01@gmail.com>
2023-04-13 19:44:23 -0300

    changing to vue

--- 01 - Music Player/scripts/music-data.js
+++ 01 - Music Player/scripts/music-data.js
@@ -1,58 +1,47 @@
-let music = {
-    name: 'Music 1',
-    artist: 'Artist 1',
-    musicLength: 5,  // 3:20 = 200
-}
+let vm = Vue.createApp({
+    data() {
+        return {
+            music: {
+                    name: 'Acorda Devinho',
+                    artist: 'Banda Rocketseat',
+                    lenght: 200,  // 3:20 = 200
+                },
+            isTimerOn: false,
+            Timer: '',
+            currentTime: 0,
+        }
+    }, 
+    methods: {
+        TimerOn: function () {
+            this.isTimerOn = true
+            
+            if(this.currentTime == 0) {
+                this.currentTime = this.music.lenght
+            }
 
-let isTimerOn = false
-let currentTime = 0
-
-const playButton = document.querySelector('#play');
-playButton.addEventListener('click', function() {
-    let timer = music.musicLength
-    TimerOn(timer)
-})
-
-
-function TimerOn(timer) {
-    isTimerOn = !isTimerOn;
-    let remainingTimeElement = document.querySelector(".remaining-time")
-    let progressFillElement = document.querySelector(".progress-bar-fill")
-    
-    if (currentTime < 0){
-        timer = music.musicLength
-    } else if(currentTime != timer) {
-        timer = currentTime
-    } 
-    
-    if(isTimerOn) {
-        playButton.src = "img/pause-solid.svg"
-
-    } else {
-        playButton.src = "img/play.svg"
-        
-    }
-    
-    let Timer = setInterval(function() {
-        if(!isTimerOn || timer < 0){
-            isTimerOn = false
-            playButton.src = "img/play.svg"
-            clearInterval(Timer)
-        } else {
-            // Se for undefined o timer acabou de ativar, se não ele já rolou antes;
-            let minutes = parseInt(timer / 60, 10)
-            let seconds = parseInt(timer % 60, 10)
+            this.Timer = setInterval(this.StartTimer, 500)
+        },
+        TimerOff: function() {
+            this.isTimerOn = false
+            clearInterval(this.Timer)
+        },
+        StartTimer: function() {
+            this.currentTime--
+        }
+    },
+    computed: {
+        TimeLeft: function() {
+            let minutes = parseInt(this.currentTime / 60, 10)
+            let seconds = parseInt(this.currentTime % 60, 10)
             
             // minutes = minutes < 10 ? "0" + minutes : minutes
             seconds = seconds < 10 ? "0" + seconds : seconds
-            
-            let percentage = parseFloat(100-(100*(timer/music.musicLength))).toFixed(2)
-            
-            progressFillElement.style.width = percentage + "%";
-            remainingTimeElement.innerText = minutes + ":" + seconds;
-            
-            timer--
-            currentTime = timer
+
+            return `${minutes}:${seconds}`
+        },
+        BarWidth: function() {
+            let width = this.currentTime == 0 ? 0 : parseFloat(100-(100*(this.currentTime/this.music.lenght))).toFixed(2)
+            return {width: width + '%'}
         }
-    }, 1000)
-}
+    }
+}).mount('.container')
