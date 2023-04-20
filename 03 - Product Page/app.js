9e605a18f4c7a4940d3b80bee0ef48167d9e9192
RennanPeron <rennanperon01@gmail.com>
2023-04-20 16:56:28 -0300

    script and style done

--- 03 - Product Page/app.js
+++ 03 - Product Page/app.js
@@ -1,3 +1,12 @@
 Vue.createApp({
-
+    data() {
+        return {
+            product: {
+                code: '42404',
+                name: 'Margot II Rose Sofa',
+                price: 4000,
+            },
+            click3D: false, 
+        }
+    },
 }).mount('#app')
